654a37906bffaa5d384920e22950ca65e4bdcda8
Osvaldo Iafelice <75169578+OzzyWez@users.noreply.github.com>
2024-02-10 01:29:54 +0100

    start lupus

---
+++ Lupus/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Lupus
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
